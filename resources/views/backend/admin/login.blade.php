f2342f7f2b058c65a97f536bb155bd5c3fe6a073
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2024-10-03 23:09:05 +0600

    admin login page some update

--- resources/views/backend/admin/login.blade.php
+++ resources/views/backend/admin/login.blade.php
@@ -79,74 +79,79 @@
     </div>
 @endsection --}}
 
-
 @extends('layouts.app')
 
+
 @section('content')
-<div class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-          <div class="card-header text-center">
-            <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-          </div>
-          <div class="card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+    <div class="hold-transition login-page">
+        <div class="login-box">
 
-            <form action="../../index3.html" method="post">
-              <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Email">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
-                  </div>
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <a href="{{ config('app.url') }}" class="h1 text-decoration-none">{{ config('app.name') }}</a>
                 </div>
-              </div>
-              <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-8">
-                  <div class="icheck-primary">
-                    <input type="checkbox" id="remember">
-                    <label for="remember">
-                      Remember Me
-                    </label>
-                  </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-4">
-                  <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                </div>
-                <!-- /.col -->
-              </div>
-            </form>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.login') }}" method="post">
+                        @csrf
 
-            <div class="social-auth-links text-center mt-2 mb-3">
-              <a href="#" class="btn btn-block btn-primary">
-                <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-              </a>
-              <a href="#" class="btn btn-block btn-danger">
-                <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-              </a>
+                        <div class="input-group mb-3">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                name="email" value="" required autocomplete="email" placeholder="Email" autofocus>
+
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password" value=""
+                                required autocomplete="current-password" placeholder="Password">
+
+
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                {{ __('Login') }}
+                            </button>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <p class="mb-1">
+                        <a href="forgot-password.html">I forgot my password</a>
+                      </p>
+                      <p class="mb-0">
+                        <a href="register.html" class="text-center">Register a new membership</a>
+                      </p>
+                </div>
             </div>
-            <!-- /.social-auth-links -->
-
-            <p class="mb-1">
-              <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-              <a href="register.html" class="text-center">Register a new membership</a>
-            </p>
-          </div>
-          <!-- /.card-body -->
         </div>
-        <!-- /.card -->
-      </div>
-</div>
+    </div>
 @endsection
